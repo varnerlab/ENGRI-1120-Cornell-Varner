697ad51af9e3f9f1d3aeefeab5b12069e6ffd77c
Varnerlab <jdv27@cornell.edu>
2021-09-13 06:17:48 -0400

    Financial examples
    examples

---
+++ src/discount_rate_example/Discount.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 30521f2c-1476-11ec-3f3e-67fe34561eea
+md"""
+#### Stuff will go here
+"""
+
+# ╔═╡ Cell order:
+# ╠═30521f2c-1476-11ec-3f3e-67fe34561eea
---
+++ src/npv_example/NPV.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ bc3df3e4-1476-11ec-1c69-dbf58e5026c1
+md"""
+Stuff will go here
+"""
+
+# ╔═╡ Cell order:
+# ╠═bc3df3e4-1476-11ec-1c69-dbf58e5026c1
