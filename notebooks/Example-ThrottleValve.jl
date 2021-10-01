5838b3483eae3591bdc389153bb611dadf472f2a
Jeffrey Varner <jdv27@cornell.edu>
2021-10-01 10:51:53 -0400

    Create Example-ThrottleValve.jl
    Starting working on TV problem

---
+++ notebooks/Example-ThrottleValve.jl
@@ -0,0 +1,38 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 26a6730a-21dd-11ec-3bbc-430e71b09495
+md"""
+
+#### Analysis of the Throttle Valve in the Vapor Compression Refrigeration Cycle (VCRC)
+
+The throttle valve 
+
+"""
+
+# ╔═╡ 8f92b67f-555a-4797-b8fb-3bea29951f25
+html"""<style>
+main {
+    max-width: 1200px;
+  	width: 80%;
+	margin: auto;
+	font-family: "Roboto, monospace";
+}
+
+a {
+	color: blue;
+	text-decoration: none;
+}
+
+.H1 {
+    padding: 0px 30px;
+}
+</style>
+"""
+
+# ╔═╡ Cell order:
+# ╠═26a6730a-21dd-11ec-3bbc-430e71b09495
+# ╟─8f92b67f-555a-4797-b8fb-3bea29951f25
