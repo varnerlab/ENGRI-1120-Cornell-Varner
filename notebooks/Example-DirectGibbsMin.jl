24c897405eb367113512ba7b234bc63e691f662a
Jeffrey Varner <jdv27@cornell.edu>
2021-10-17 10:10:01 -0400

    Create Example-DirectGibbsMin.jl
    Starting on Direct Gibbs min

---
+++ notebooks/Example-DirectGibbsMin.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3b5be900-2f53-11ec-1477-d93bea95f307
+
+
+# ╔═╡ Cell order:
+# ╠═3b5be900-2f53-11ec-1477-d93bea95f307
