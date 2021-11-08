a84a0b7d7e46e051a0b2c111660510c6c6ce0532
Varnerlab <jdv27@cornell.edu>
2021-11-08 16:29:03 -0500

    Hmmm
    not sure what this is

--- prelims/P2/practice/Practice-Prelim-2-ENGRI-1120-F21-RProblem.jl
+++ prelims/P2/practice/Practice-Prelim-2-ENGRI-1120-F21-RProblem.jl
@@ -20,13 +20,13 @@ The substrate $A$ (component 1) is supplied to the reactor at 10 mol/hr, where t
 __Assumptions__:
 * The CSTR is well-mixed, operating at steady-state, and has a constant volume V = 14L
 * The CSTR has a single input stream and a single output stream
-* Neither of the chemical reactions occurs in the streams
+* Neither of the chemical reactions occur in the streams
 * There is no $B$ or $C$ in the input stream
 
 __Compute__:
 * The chemical reaction rate(s) $\hat{r}_{1}$ and $\hat{r}_{2}$
 * The mol flow rates of all components in the CSTR exit stream
-* If $\hat{r}_{1}$ and $\hat{r}_{2}$ and irreversible, and first-order in $A$, compute the forward rate constants $k_{1}$ and $k_{2}$ for reaction 1 and 2, respectively. 
+* If $\hat{r}_{1}$ and $\hat{r}_{2}$ are irreversible, and first-order in $A$, compute the forward rate constants $k_{1}$ and $k_{2}$ for reaction 1 and 2, respectively. 
 
 """
 
